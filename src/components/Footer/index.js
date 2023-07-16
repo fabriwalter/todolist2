8b6f75ec632476493e39324197b4af3d973eca1b
Walter Fabri <walterfabri18@gmail.com>
2023-07-16 00:55:35 -0300

    finalizado design da pagina home

---
+++ src/components/Footer/index.js
@@ -0,0 +1,24 @@
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
+import './footer.css';
+
+export default function Footer() {
+    return(
+        <footer className='footer'>
+            
+            <hr></hr>
+            
+            <div className='div-footer'>
+                
+                <span className='title-footer'>Designed by Walter Fabri</span>
+                
+                <div className='div-icons'>
+                    <Link to="https://github.com/fabriwalter" className='github-icon'><FaGithub /></Link>
+                    <Link to="https://www.linkedin.com/in/walter-fabri-3426638a/" className='linkedin-icon'><FaLinkedin /></Link>
+                </div>
+            </div>
+            
+        </footer>
+    );
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+
+import { Link } from 'react-router-dom';
 
 import './home.css';
 
@@ -11,6 +14,20 @@ export default function Home() {
             <div className='home-subtitulo'>
                 <p className='roboto-light'>Simplifique sua rotina: o aplicativo inteligente que mantém suas tarefas em ordem.</p>
             </div>
+
+            <form className='home-form'>
+                <input type='text' placeholder='Digite seu e-mail...' className='input-email' />
+                <input type='text' placeholder='********************' className='input-senha' autoComplete={false} />
+                
+
+                <button type='submit' className='button-acessar'>Acessar</button>
+            </form>
+
+            <Link to="/register" className='link-para-register'>
+                Não possui uma conta? Cadastre-se!
+            </Link>
+
+            <Footer />
         </div>
     );
 }
